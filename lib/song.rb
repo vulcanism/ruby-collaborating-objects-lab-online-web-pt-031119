497e2193e3baf3bea8866d80d3ad92f5d455eae6
Savannah C <sgcorrell@gmail.com>
2019-04-10 16:37:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
     song = filename.split(" - ")[1]
     artist = filename.split(" - ")[0]
     new_song = self.new(song)
-    new_song.artist_name = artist
+    new_song.artist = artist
     new_song
   end
   
