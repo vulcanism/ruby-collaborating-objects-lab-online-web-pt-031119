b0abff71b6958d3cfa0bdd25f07fb94d8d853a25
Savannah C <sgcorrell@gmail.com>
2019-04-10 16:29:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,4 +24,12 @@ class Artist
     self.find(name) ? self.find(name) : self.create(name)
   end
   
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
+  end
+  
 end
